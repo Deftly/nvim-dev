17ecb40c3136c4e468c90ef02a479c81ab969ba1
Deftly <deftkz@gmail.com>
2022-12-22 09:42:43 -0800

    initial config layout and autocommands

---
+++ init.lua
@@ -0,0 +1,10 @@
+ -- Load global functions
+require("globals")
+-- Plugin management via Packer
+require("plugins")
+-- "Global" Keymappings
+require("mappings")
+-- All non plugin related (nvim) options
+require("options")
+-- vim autocommands
+require("autocmd")
---
+++ lua/autocmd.lua
@@ -0,0 +1,63 @@
+local api = vim.api
+local settings = require("settings")
+
+-- Remove all trailing whitespace on save
+local TrimWhiteSpaceGrp = api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
+api.nvim_create_autocmd("BufWritePre", {
+  command = [[:%s/\s\+$//e]],
+  group = TrimWhiteSpaceGrp,
+})
+
+-- Don't auto comment new line
+api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
+-- wrap words "softly" (no carriage return) in mail buffer
+api.nvim_create_autocmd("Filetype", {
+  pattern = "mail",
+  callback = function()
+    vim.opt.textwidth = 0
+    vim.opt.wrapmargin = 0
+    vim.opt.wrap = true
+    vim.opt.linebreak = true
+    vim.opt.columns = 80
+    vim.opt.colorcolumn = "80"
+  end,
+})
+
+-- Highlight on yank
+local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+  command = "silent! lua vim.highlight.onyank()",
+  group = yankGrp
+})
+-- Go to last location when opening a buffer
+api.nvim_create_autocmd(
+  "BufReadPost",
+  { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
+)
+-- windows to close with "q"
+api.nvim_create_autocmd("Filetype", {
+  pattern = { "help", "startuptime", "qf", "fugitive", "null-ls-info", "dap-float" },
+  command = [[nnoremap <buffer><silent> q :close<CR>]],
+})
+api.nvim_create_autocmd("FileType", { pattern = "man", command = [[nnoremap <buffer><silent> q :quit<CR>]]})
+
+-- Show cursor line only in active window
+local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
+api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  pattern = "*",
+  command = "set cursorline",
+  group = cursorGrp,
+})
+api.nvim_create_autocmd(
+  { "InsertEnter", "WinLeave" },
+  { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
+
+
+
+
+
+
+
