36b80d22fd552ef6b081172f9926339c253fd11f
Deftly <deftkz@gmail.com>
2022-12-22 16:14:03 -0800

    alphabetical reordering

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
+-- vim autocommands
+require("project.autocmd")
 -- Load global functions
 require("project.globals")
--- Plugin management via Packer
-require("project.plugins")
 -- "Global" Keymappings
 require("project.mappings")
 -- All non plugin related (nvim) options
 require("project.options")
--- vim autocommands
-require("project.autocmd")
+-- Plugin management via Packer
+require("project.plugins")
